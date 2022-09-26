dcbf40e6f58c87e2ff7951bbacc98f6fda9a8668
Corina <corbadesign@gmail.com>
2022-09-26 18:23:58 +0200

    Php files

---
+++ Compushop C.V.C/nieuws.php
@@ -0,0 +1,90 @@
+<<!doctype html>
+<!--test-->
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compushop C.V.C | Nieuws</title>
+    <link rel="stylesheet" href="../css/global.css">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="../css/nieuws.css">
+</head>
+
+<body>
+    <!-- <header class="nav sticky-top"> 
+        Hier wil ik nog zorgen dat de header sticky blijft)
+    -->
+    <img class="nav sticky-top bd-placeholder-img-lg img-fluid" id="showcase" src="../img/menu.jpg" alt="menu image">
+
+    <nav class="menu">
+        <li><a href="./index.html" data-hover="Home">Home</a></li>
+        <li><a href="./product_keuzen.html" data-hover="Producten">Producten</a></li>
+        <li><a href="./nieuws.html" data-hover="Nieuws">Nieuws</a></li>
+        <li><a href="./contact.html" data-hover="Contact">Contact</a></li>
+        <li><a href="./over_ons.html" data-hover="Over ons">Over ons</a></li>
+    </nav>
+    <!-- </header> -->
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <img src="/img/menu.jpg" class="card-img-top" alt="menu image" />
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Nieuwe site is een feit</h5>
+                        <p class="card-text">
+                            Ik mag wel zeggen dat het bloed, zweet en tranen heeft gekost, maar onze nieuwe website is
+                            een feit.
+                            Ik denk dat ik voor het hele team kan spreken dat we trots mogen zijn op onze website.
+                            Persoonlijk heb ik best veel geleerd tijdens het bouwen van deze website.
+                            Niet alleen heb ik de voordelen van bootstrap ervaren. Ook het werken in teamverband om
+                            samen tot een goed resultaat te komen was een nieuwe positieve ervaring voor mij.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="/img/chip.png" class="card-img-top img_chip"
+                        alt="chip" />
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                            content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <!-- <div class="card">
+                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" class="card-img-top"
+                        alt="Fissure in Sandstone" />
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                            content.</p>
+                        <a href="#!" class="btn btn-primary">Button</a>
+                    </div>
+                </div> -->
+            </div>
+        </div>
+    </div>
+</body>
+<footer>
+    <section id="newsletter">
+        <div class="container">
+            <h1>Subscribe to me</h1>
+            <form>
+                <input type="email" placeholder="Enter Email...">
+                <button type="submit" class="newsletter_button">Subscribe</button>
+            </form>
+        </div>
+    </section>
+    <p>Copyright 2022 © Compushop C.V.C</p>
+    </div>
+</footer>
+
+
+</html>
